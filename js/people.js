ff6c8fbdc0a9708c8bf07496f0c4b90ee8ed6c58
DamianoMura <mura.damiano14@gmail.com>
2025-07-29 21:38:01 +0200

    [add] created function on hobbies

--- js/people.js
+++ js/people.js
@@ -1,3 +1,7 @@
 const people = require('./names.js');
 
 console.log(people('Damiano','Mura'))
+
+const hobbies = require('./hobbies.js');
+
+console.log(hobbies('Guitar','videogames', 'programming' ))
